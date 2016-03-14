a95e00638477d56af16374152315854014af5f20
Ben Ebel <bge1234@gmail.com>
2016-03-14 11:17:15 -0600

    connected up mongo db and can create a new document in the 'foods' collection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var URL = 'mongodb://localhost/piksil-storage';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api', function(req, res) {
+  MongoClient.connect(URL, function(err, db) {
+    if (err) return
+
+    var collection = db.collection('foods');
+    collection.insert([{name: 'taco', tasty: true}, {name: 'taco2', tasty: false}], function(err, result) {
+      collection.find().toArray(function(err, docs) {
+        db.close();
+        res.json(docs);
+      });
+    });
+  });
 });
 
 module.exports = router;
